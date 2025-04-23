3e679726a167d3e8bfd15602fca5e598c07b3e7f
Bhakti Budiman Novanda <myforework@live.com>
2025-04-23 18:08:36 +0700

    feat: Tambahkan dependensi crypto-js dan tipe terkait pada package.json dan package-lock.json, serta perbarui file tsconfig.build.tsbuildinfo untuk mencerminkan perubahan

--- dist/admin/admin.controller.d.ts
+++ dist/admin/admin.controller.d.ts
@@ -11,21 +11,21 @@ export declare class AdminController {
         apiKeys: {
             name: string;
             id: number;
-            companyId: number;
             createdAt: Date;
             updatedAt: Date;
-            key: string;
+            companyId: number;
             description: string | null;
             isActive: boolean;
+            key: string;
             lastUsedAt: Date | null;
             expiresAt: Date | null;
         }[];
         name: string;
         id: number;
-        userId: number;
-        companyId: number | null;
         createdAt: Date;
         updatedAt: Date;
+        userId: number;
+        companyId: number | null;
         user: Partial<User>;
         company?: {
             apiKeys: ApiKey[];
